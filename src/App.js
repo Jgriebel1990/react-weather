6c144818c35fcb22844260b79cbe270e808ccd83
jefferson Griebel <jefferson.griebel@gmail.com>
2018-02-21 18:15:52 -0500

    added min and max lat and lon and added required so you have to have coordinates to find the weather. deleted old read.me

--- src/App.js
+++ src/App.js
@@ -1,27 +1,26 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
-  constructor(){
+  constructor() {
     super();
     this.state = {
       lat: 0,
-      lon: 0,
-
+      lon: 0
     };
     this.handleLatChange = this.handleLatChange.bind(this);
     this.handleLonChange = this.handleLonChange.bind(this);
   }
 
-  handleLatChange(e){
+  handleLatChange(e) {
     this.setState({
       lat: +e.target.value
-    })
+    });
   }
-  handleLonChange(e){
+  handleLonChange(e) {
     this.setState({
       lon: +e.target.value
-    })
+    });
   }
   render() {
     return (
@@ -31,11 +30,23 @@ class App extends Component {
         <form>
           <label>
             Latitude:
-            <input type="text" onChange={(e) => this.handleLatChange(e)} value={this.state.lat}/>
+            <input
+              type="number"
+              min="-90"
+              max="90"
+              onChange={e => this.handleLatChange(e)}
+              value={this.state.lat}
+              required />
           </label>
           <label>
             Longitude:
-            <input type="text" onChange={(e) => this.handleLonChange(e)} value={this.state.lon}/>
+            <input
+              type="number"
+              min="-180"
+              max="180"
+              onChange={e => this.handleLonChange(e)}
+              value={this.state.lon}
+              required />
           </label>
           <button type="submit">WEATHER!!!</button>
         </form>
