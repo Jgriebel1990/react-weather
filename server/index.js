1dad1f3db56f26be7cb1a4d0a11e3bbcf5cf5c09
jefferson Griebel <jefferson.griebel@gmail.com>
2018-02-21 20:26:09 -0500

    made the request of weather data through the proxy service dynamic based on the route params latitude and longitude

--- server/index.js
+++ server/index.js
@@ -3,23 +3,23 @@ const express = require("express");
 const axios = require("axios");
 
 //configuration
-require('dotenv').config();
- const { API_KEY } = process.env
- console.log(API_KEY);
+require("dotenv").config();
+const { API_KEY } = process.env;
+console.log(API_KEY);
 //creation of stuff
 const serverApp = express();
 const port = process.env.PORT || 5000;
 
 serverApp.get("/forecast/:lat,:lon", function(request, response) {
-  const url =
-    `https://api.darksky.net/forecast/${API_KEY}/37.8267,-122.4233`;
+  const { lat, lon } = request.params;
+  const url = `https://api.darksky.net/forecast/${API_KEY}/${lat},${lon}`;
   axios
     .get(url)
     .then(res => {
-      response.json(res.data);
+      response.status(200).json(res.data);
     })
     .catch(err => {
-      response.json({
+      response.status(500).json({
         msg: "yur stuff is broke"
       });
     });
