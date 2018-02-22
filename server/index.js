2b7c66acdebf0e8f417edfb513813befee9e2cc5
jefferson Griebel <jefferson.griebel@gmail.com>
2018-02-21 19:58:07 -0500

    created .env and .env-template to hide our files

---
+++ server/index.js
@@ -0,0 +1,30 @@
+//imports
+const express = require("express");
+const axios = require("axios");
+
+//configuration
+require('dotenv').config();
+ const { API_KEY } = process.env
+ console.log(API_KEY);
+//creation of stuff
+const serverApp = express();
+const port = process.env.PORT || 5000;
+
+serverApp.get("/forecast/:lat,:lon", function(request, response) {
+  const url =
+    `https://api.darksky.net/forecast/${API_KEY}/37.8267,-122.4233`;
+  axios
+    .get(url)
+    .then(res => {
+      response.json(res.data);
+    })
+    .catch(err => {
+      response.json({
+        msg: "yur stuff is broke"
+      });
+    });
+});
+
+serverApp.listen(port, () => {
+  console.log(`Now listening on port ${port}`);
+});
