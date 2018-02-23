b87224f113df3936f249f32b8aa5c8c1a31b2c1c
jefferson Griebel <jefferson.griebel@gmail.com>
2018-02-22 20:36:38 -0500

    set up image selection for respective icons

---
+++ client/src/images/index.js
@@ -0,0 +1,14 @@
+import rain from './pngrain.png'
+
+
+
+
+const selection = (name) => {
+    switch(name){
+        case 'rain':
+            return rain
+        default:
+            return rain
+    }
+}
+export default selection;
